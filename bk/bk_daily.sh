96f16e48917f249e9fc70a22942e8eac1d44b362
qcmarcel <qc.marcel@gmail.com>
2017-08-12 20:31:58 -0500

    autobono 2

--- bk/bk_daily.sh
+++ bk/bk_daily.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #echo "creando dump";
 #dir=$DIRSTACK"/erp.franchise/bk/";
-dir=$HOME"/html/erp.franchise/bk/";
-hostname=$1;
-username=$2;
-password=$3;
-database=$4;
-crear=$(date +"%Y%m%d" )"_"$database; # echo ("en local --date='-1 day'");
-eliminar=$(date +"%Y%m%d" --date='-15 day')"_"$database; 
-#echo $archivo;
-mysqldump -h $hostname -u $username -p$password $database > $dir$crear"_dump.sql";
+dir=$HOME"/html/erp.franchise/bk/"
+hostname=$1
+username=$2
+password=$3
+database=$4
+crear=$(date +"%Y%m%d" )"_"$database
+# echo ("en local --date='-1 day'");
+eliminar=$(date +"%Y%m%d" --date='-15 day')"_"$database
+mysqldump -h $hostname -u $username -p$password $database > $dir$crear"_dump.sql"
 if [[ $(find $dir*.sql | wc -l) -ge 15 ]]; then
-	rm $dir$eliminar"_dump.sql";
-	#rm $dir$crear"_dump.sql";
+        rm $dir$eliminar"_dump.sql";
+        #rm $dir$crear"_dump.sql";
 fi
-rm $dir"db_access.php";
